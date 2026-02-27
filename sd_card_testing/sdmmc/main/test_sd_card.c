c96ae00f65a1b295bd2a0e5b29db46b1b567c1cd
f-noble <136553218+f-noble@users.noreply.github.com>
2026-02-26 19:13:07 -0500

    isolating code to mount and unmount

--- sd_card_testing/sdmmc/main/test_sd_card.c
+++ sd_card_testing/sdmmc/main/test_sd_card.c
@@ -2,12 +2,31 @@
 
 */
 
-#include "esp_log.h"
-#include "sd_card_pins.h"
+#include <sys/unistd.h> // lets you pass to stdout
+#include "esp_log.h" // print to esp monitor
+#include "esp_err.h" // run esp code in error catcher macro
+#include "esp_vfs_fat.h" // mounting and unmounting fat filesystem
+#include "sd_card_pins.h" // edit this file to configure pins
 
 static const char *LOGTAG = "test_sd";
 
 void app_main(void)
 {
   ESP_LOGI(LOGTAG, "Hello world!");
+
 }
+
+
+
+
+
+// Things needed to mount and unmount
+// basically we need everything that is passed into this function to successfully mount.
+// 
+// esp_err_t mount_successful = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
+// ESP_LOGE(LOGTAG, "Attempting to mount sd card. Showing: %s", esp_err_to_name(mount_successful))
+// sdmmc_card_print_info(stdout, card); // make sure card is mounted
+// 
+//ESP_ERROR_CHECK(
+// esp_vfs_fat_sdcard_unmount(mount_point, card)
+// ); // all-in-one unmount
